e0123f053fc74148205c902a5fe545083118f4ea
YeoleKrushna <yeolekrushna00@gmail.com>
2025-04-20 01:44:54 +0530

    commit

--- yeole/settings.py
+++ yeole/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-q!rykrz@^!$zmr*9f@c9%=%56n7^#u@5$*1w-%4j3rwd9algli
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yeole-django-web.onrender.com']
 
 
 # Application definition
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -111,6 +112,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
